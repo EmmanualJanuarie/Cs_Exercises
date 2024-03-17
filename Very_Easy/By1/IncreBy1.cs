911c70f123b64a2d32f8fa53f93510d18e63617a
Emmanual <emmanualjanuarie240@gmail.com>
2024-03-17 15:06:49 +0200

    Add new folder with additional files: By1

---
+++ Very_Easy/By1/IncreBy1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+//~This program is created to increment the useres entry by 1
+
+//creating the namespace
+namespace Math
+{
+    //Fabricating a class
+    class IncrementBy1
+    {
+        //creating the "Main" method
+        static void Main(string[] args)
+        {
+            //create the variable to allow user to enter a value
+            Console.WriteLine("Please enter a value:");
+            string? strNum = Console.ReadLine();
+
+            //converting the string num to int
+            if(int.TryParse(strNum, out int intNum))
+            {
+                //return to the console the value
+                Console.WriteLine("Result:" + By1(intNum));
+            }
+        }
+
+
+        //creating a method to handle the incrementation
+        static int By1(int intNum)
+        {
+            return intNum + 1;
+        }
+    }
+}

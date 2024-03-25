9bc225634a9e94b65abc3eec964f15da04a67dd3
Emmanual <emmanualjanuarie240@gmail.com>
2024-03-25 13:40:06 +0200

    Add new folder CheckForNum

---
+++ Very_Easy/CheckForNum/CheckNum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Transactions;
+
+namespace lang_fundamentals
+{
+    class CheckNum
+    {
+    static void Main(string[] args)
+        {
+            try
+            {
+                //creating a global variable
+                Console.WriteLine("Enter a value: ");
+                String str_Num = Console.ReadLine();
+                //converting to int num
+                if(int.TryParse(str_Num, out int int_Num))
+                {
+                    //printing to the terminal 
+                    Console.WriteLine("Entered value is: " + int_Num);
+                }else if(!int.TryParse(str_Num,out _))
+                {
+                    throw new Exception("Please enter a number Not a string!");
+                }
+            }
+            catch (Exception e)
+            {
+                 Console.WriteLine("Error message: " + e.Message);
+            }
+        }
+
+    }
+}

c5880b3ffd1dd2ef7b181b3cc0a634ef4e711c04
Emmanual <emmanualjanuarie240@gmail.com>
2024-03-25 15:17:25 +0200

    Added files to Ages_To_Days folder

---
+++ Very_Easy/Age_To_DAYS/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Math
+{
+    //creating a class
+class ToDays    
+{
+    //creating a variable 
+    int var = 10;
+    //creating a method to house the algorthm 
+    static void Age_To_Days(int intVal)
+    {
+        return intVal * 365;
+    }
+    //creating the Main method
+    static void Main(string[] args)
+    {
+      //calling the method
+        Age_To_Days(var);
+    }
+}
+
+}
+

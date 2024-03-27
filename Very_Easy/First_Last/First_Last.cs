140424d54e783a293682d5a6a3cfefed0955cb6d
Emmanual <emmanualjanuarie240@gmail.com>
2024-03-27 22:52:22 +0200

    Add folder called First_Last with extra content

---
+++ Very_Easy/First_Last/First_Last.cs
@@ -0,0 +1,23 @@
+﻿namespace arrays
+{
+    //creating a class 
+    class First_Last
+    { 
+        //creating the method for first and last items in array
+        static void First_Last_array(int firstItem , int lastItem)
+        {
+            Console.WriteLine($"Returned: [{firstItem},{lastItem}]");
+        }
+        static void Main(string[] args)
+        {
+           int[] myArray = {10, 20, 30, 40 , 50 , 60, 70 , 80, 90, 100};
+            // creating a vartiable for the first item in the array
+            int item_First = myArray[0];
+            // creating a vartiable for the last item in the array
+            int item_Last = myArray[myArray.Length -1];
+
+            //calling the First_Last method
+          First_Last_array(item_First, item_Last);
+        }
+    }
+} 

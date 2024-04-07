25b172cbb215974892982883ea4e3df95179403a
Emmanual <emmanualjanuarie240@gmail.com>
2024-04-07 20:57:46 +0200

    Add new folder ValidZipCode

---
+++ Easy/ValidZipCode/Program.cs
@@ -0,0 +1,51 @@
+﻿//creating namespace for ValidZipCode
+namespace String
+{
+    //creating class for ValidZipCode
+    class ValidZipCode
+    {
+        //creating the Main method
+        static void Main(string[] args)
+        {
+            //Creating the input variable and input message
+            Console.Write("Please enter zip code: ");
+            string str_ZipCode = Console.ReadLine();
+
+            //calling the method
+            Console.Write(IsValidZipCode(str_ZipCode));
+        }
+
+        //creating the method for ValidZip code detector
+        static string IsValidZipCode(string ZipCode)
+        {
+            //creating ctemp variable
+            string result = " ";
+            //creating length variable
+            int length = ZipCode.Length;
+            foreach (char x in ZipCode)
+            {
+                if(!char.IsDigit(x))
+                {
+                    result += "False";
+                    break;
+                }
+                else if(length > 5)
+                {
+                     result += "False";
+                     break;
+                }
+                 else if(ZipCode.Contains(' '))
+                {
+                     result += "False";
+                     break;
+                }
+                else
+                {
+                    result += "True";
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
